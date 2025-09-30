6959790e6717f24f6b3d18682e03567162364e4b
William Zetterlund <zetterlundwa@gmail.com>
2025-09-29 21:02:50 -0700

    Update ParityCauldrons.java

--- snookie/paritycauldrons/ParityCauldrons.java
+++ snookie/paritycauldrons/ParityCauldrons.java
@@ -1,1 +1,24 @@
+package com.snookie.paritycauldrons;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ParityCauldrons extends JavaPlugin {
+
+    private static ParityCauldrons instance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        getLogger().info("ParityCauldrons has been enabled.");
+        // We'll register event listeners here later
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("ParityCauldrons has been disabled.");
+    }
+
+    public static ParityCauldrons getInstance() {
+        return instance;
+    }
+}
