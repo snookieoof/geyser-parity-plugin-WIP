94254b18c28396a280d4fefd2af83d0c9223e1b4
William Zetterlund <zetterlundwa@gmail.com>
2025-09-29 21:10:23 -0700

    Update CauldronInteractListener.java
    added empting

--- snookie/paritycauldrons/listeners/CauldronInteractListener.java
+++ snookie/paritycauldrons/listeners/CauldronInteractListener.java
@@ -1,47 +1,47 @@
-package com.william.paritycauldrons.listeners;
+@EventHandler
+public void onPlayerInteract(PlayerInteractEvent event) {
+    if (event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
 
-import com.william.paritycauldrons.ParityCauldrons;
-import com.william.paritycauldrons.CauldronManager;
-import com.william.paritycauldrons.CauldronType;
-import org.bukkit.*;
-import org.bukkit.block.Block;
-import org.bukkit.entity.Player;
-import org.bukkit.event.Listener;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.event.block.Action;
+    Block block = event.getClickedBlock();
+    if (block == null || block.getType() != Material.CAULDRON) return;
 
-public class CauldronInteractListener implements Listener {
+    Player player = event.getPlayer();
+    ItemStack item = player.getInventory().getItem(event.getHand());
+    if (item == null) return;
 
-    @EventHandler
-    public void onPlayerInteract(PlayerInteractEvent event) {
-        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+    // Filling with lava
+    if (item.getType() == Material.LAVA_BUCKET) {
+        CauldronManager.setCauldronType(block, CauldronType.LAVA);
+        player.getInventory().setItem(event.getHand(), new ItemStack(Material.BUCKET));
+        block.getWorld().spawnParticle(Particle.FLAME, block.getLocation().add(0.5, 1, 0.5), 10);
+        player.sendMessage(ChatColor.RED + "Cauldron filled with lava.");
+        event.setCancelled(true);
+    }
 
-        Block block = event.getClickedBlock();
-        if (block == null || block.getType() != Material.CAULDRON) return;
+    // Filling with powdered snow
+    if (item.getType() == Material.POWDER_SNOW_BUCKET) {
+        CauldronManager.setCauldronType(block, CauldronType.POWDERED_SNOW);
+        player.getInventory().setItem(event.getHand(), new ItemStack(Material.BUCKET));
+        block.getWorld().spawnParticle(Particle.SNOWFLAKE, block.getLocation().add(0.5, 1, 0.5), 10);
+        player.sendMessage(ChatColor.AQUA + "Cauldron filled with powdered snow.");
+        event.setCancelled(true);
+    }
 
-        Player player = event.getPlayer();
-        ItemStack item = player.getInventory().getItem(event.getHand());
+    // Emptying lava cauldron
+    if (item.getType() == Material.BUCKET && CauldronManager.getCauldronType(block) == CauldronType.LAVA) {
+        player.getInventory().setItem(event.getHand(), new ItemStack(Material.LAVA_BUCKET));
+        CauldronManager.setCauldronType(block, CauldronType.WATER);
+        block.getWorld().spawnParticle(Particle.SMOKE_LARGE, block.getLocation().add(0.5, 1, 0.5), 10);
+        player.sendMessage(ChatColor.GRAY + "You scooped lava from the cauldron.");
+        event.setCancelled(true);
+    }
 
-        if (item == null) return;
-
-        // Lava bucket interaction
-        if (item.getType() == Material.LAVA_BUCKET) {
-            CauldronManager.setCauldronType(block, CauldronType.LAVA);
-            player.getInventory().setItem(event.getHand(), new ItemStack(Material.BUCKET));
-            block.getWorld().spawnParticle(Particle.FLAME, block.getLocation().add(0.5, 1, 0.5), 10);
-            player.sendMessage(ChatColor.RED + "Cauldron filled with lava.");
-            event.setCancelled(true);
-        }
-
-        // Powdered snow bucket interaction
-        if (item.getType() == Material.POWDER_SNOW_BUCKET) {
-            CauldronManager.setCauldronType(block, CauldronType.POWDERED_SNOW);
-            player.getInventory().setItem(event.getHand(), new ItemStack(Material.BUCKET));
-            block.getWorld().spawnParticle(Particle.SNOWFLAKE, block.getLocation().add(0.5, 1, 0.5), 10);
-            player.sendMessage(ChatColor.AQUA + "Cauldron filled with powdered snow.");
-            event.setCancelled(true);
-        }
+    // Emptying powdered snow cauldron
+    if (item.getType() == Material.BUCKET && CauldronManager.getCauldronType(block) == CauldronType.POWDERED_SNOW) {
+        player.getInventory().setItem(event.getHand(), new ItemStack(Material.POWDER_SNOW_BUCKET));
+        CauldronManager.setCauldronType(block, CauldronType.WATER);
+        block.getWorld().spawnParticle(Particle.CLOUD, block.getLocation().add(0.5, 1, 0.5), 10);
+        player.sendMessage(ChatColor.GRAY + "You scooped powdered snow from the cauldron.");
+        event.setCancelled(true);
     }
 }
